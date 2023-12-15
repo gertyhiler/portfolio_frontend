1a48e4c3bbb2a7fa81efa15b6806a2e1e77c3550
gertyhiler <tryuserrus@gmail.com>
2023-12-15 21:10:37 +0300

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { WorksList } from "@/components/WorksList";
-import {Hero} from "@/components/Hero/Hero";
+import WorksList from "@/components/WorksList/WorksList";
+import { Hero } from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <>
-        <section className="work">
-            <Hero/>
-            <WorksList/>
-        </section>
+      <section className="work">
+        <Hero />
+        <WorksList />
+      </section>
     </>
   );
 }
