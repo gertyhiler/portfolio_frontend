bd3e2dc2388750cfa5b4445fdbb7b8e0d0caf8df
gertyhiler <tryuserrus@gmail.com>
2023-12-15 21:10:14 +0300

    formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,42 @@
-import './globals.css'
-import 'animate.css'
-import type {Metadata} from 'next'
-import {Montserrat} from 'next/font/google'
-import {Header} from '@/components/Header'
+import "./globals.css";
+import "animate.css";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import { Header } from "@/components/Header";
 import React from "react";
-import {Footer} from "@/components/Footer/Footer";
+import { Footer } from "@/components/Footer/Footer";
 import Script from "next/script";
-import {SeoLinks} from "@/components/SeoLinks/SeoLinks";
+import { SeoLinks } from "@/components/SeoLinks/SeoLinks";
 
-const font = Montserrat({subsets: ['latin']})
+const font = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: 'Портфолио | Коробка Андрей',
-    description: 'Список всех мной реализованных проектов',
-    themeColor: "#1F2937"
-}
+  title: "Портфолио | Коробка Андрей",
+  description: "Список всех мной реализованных проектов",
+  themeColor: "#1F2937",
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="ru">
-        <Script src='/ya.metrica.js'/>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2004SZCNZX"/>
-        <Script src="/ga-tag.js"/>
-        <SeoLinks/>
-        <body className={`${font.className} dark:bg-gray-900 px-2 md:px-6 animate__animated animate__fadeIn`}>
-        <Header/>
-        <main>
-            {children}
-        </main>
-        <Footer/>
-        </body>
-        </html>
-    )
+  return (
+    <html lang="ru">
+      <Script src="/ya.metrica.js" />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2004SZCNZX"
+      />
+      <Script src="/ga-tag.js" />
+      <SeoLinks />
+      <body
+        className={`${font.className} dark:bg-gray-900 px-2 md:px-6 animate__animated animate__fadeIn`}
+      >
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 }
