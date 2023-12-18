f3b29f117d877cbb70b1bf0253e068222f3adcd4
gertyhiler <tryuserrus@gmail.com>
2023-12-18 13:42:08 +0300

    add profile fetcher

--- src/$api/getData.ts
+++ src/$api/getData.ts
@@ -1,9 +1,9 @@
-import { apiResponse } from "@/types/api";
+import { ProfileResponse, WorkResponse } from "@/types/api";
 
 export const API_URL = process.env.API_URL || "http://localhost:1337";
-export async function getData(): Promise<apiResponse[]> {
+export async function getData(): Promise<WorkResponse[]> {
   const res = await fetch(
-    `${API_URL}/api/works/?populate[teches][populate][0]=logo&populate=screenshot`,
+    `${API_URL}/api/works/?sort=position&populate[teches][populate][0]=logo&populate=screenshot`,
     { next: { revalidate: 600 } }
   );
   if (!res.ok) {
@@ -12,3 +12,16 @@ export async function getData(): Promise<apiResponse[]> {
 
   return (await res.json()).data;
 }
+
+export async function getProfile(): Promise<ProfileResponse> {
+  const res = await fetch(
+    `${API_URL}/api/profile?populate=*`,
+    { next: { revalidate: 600 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return (await res.json());
+}
+
