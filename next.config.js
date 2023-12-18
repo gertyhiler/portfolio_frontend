69f86f2e0569dc086f6cfa2e5ebb80ccd5b1128d
gertyhiler <tryuserrus@gmail.com>
2023-12-18 13:40:55 +0300

    add remote path to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "serv1.andrew-web.ru",
+        port: "",
+        pathname: "/uploads/**",
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
