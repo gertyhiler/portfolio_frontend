55202155c70a39ea539990dd70c5dc0749442135
gertyhiler <tryuserrus@gmail.com>
2023-12-19 23:12:26 +0300

    add next current path to headers

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export default function middleware(request: NextRequest) {
+  return NextResponse.next({
+    request: {
+      headers: new Headers({ "x-next-path-page": request.nextUrl.pathname }),
+    },
+  });
+}
+
+export const config = {
+  matcher: '/',
+}
