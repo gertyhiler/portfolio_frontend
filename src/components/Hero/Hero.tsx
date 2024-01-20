b853d7217fca3afde7309076b515d0f65f62c8ef
gertyhiler <tryuserrus@gmail.com>
2024-01-20 23:09:13 +0300

    change layout and links of hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,25 +6,28 @@ export function Hero() {
   return (
     <section className="bg-white dark:bg-gray-900" id="about">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7 animate__animated animate__fadeInLeft">
-          <h2 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+        <div className="mx-auto place-self-center col-span-12 animate__animated animate__fadeInLeft">
+          <h2 className="max-w-full text-center mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
             Hi, 👋
             <p className="text-purple-600 border-primary-600 dark:text-purple-600">
               Я Андрей
             </p>
             Frontend developer
           </h2>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="max-w-6xl text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             Мой стек: React + Typescript + NodeJs. По мимо фронтенд разработки
             развиваюсь в маркетинге и дизайне.
             <br />
             Люблю вино 🍷 и баскетбол 🏀
           </p>
+          <div className="flex justify-center items-center">
           <Link
-            href="#projects"
+            href="http://t.me/bananawins"
+            target="_blank"
+            referrerPolicy="no-referrer"
             className="inline-flex items-center justify-center px-5 py-3 mr-3 transition-colors text-base font-medium text-center text-white rounded-lg bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 dark:focus:ring-purple-900"
           >
-            Проекты
+            Написать
             <svg
               className="w-5 h-5 ml-2 -mr-1"
               fill="currentColor"
@@ -39,17 +42,14 @@ export function Hero() {
             </svg>
           </Link>
           <Link
-            href="http://t.me/bananawinst"
-            target="_blank"
+            href="/blog"
             className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center transition-colors text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            referrerPolicy="no-referrer"
           >
-            Написать мне
+            Блог
           </Link>
+          </div>
+          
         </div>
-        {/* <div className="hidden lg:mt-0 lg:col-span-5 lg:flex animate__animated animate__fadeInRight">
-          <Image src={certificate} className="max-w-[200px] h-auto" alt="Сертификат react" />
-        </div> */}
       </div>
     </section>
   );
