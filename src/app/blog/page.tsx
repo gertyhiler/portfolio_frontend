a6204bf517bf733d9b5a1d593fe858a5be3588e0
gertyhiler <tryuserrus@gmail.com>
2024-01-20 23:14:33 +0300

    render empty section component if articles is empty

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,7 @@ import { getArticles } from "@/$api/getData";
 import { type FC } from "react";
 import { WorkCard } from "@/components/WorkCard";
 import { BACKEND_URL } from "@/constant/back-route";
+import EmptySection from "@/components/EmtySecton";
 
 interface BlogPageProps {}
 
@@ -13,15 +14,28 @@ const BlogPage: FC<BlogPageProps> = async () => {
         <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
           Блог
         </h2>
-        {/* <p className="mt-1 text-gray-600 dark:text-gray-400">
-          Выбирай, что хочешь почитать сегодня
-        </p> */}
       </div>
-      <ul className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {articles.data.map((article) => (
-          <WorkCard variant="blog" key={article.id} headline={article.attributes.title} description={article.attributes.seo.metaDescription || ""} link={`/blog/${article.attributes.slug}`} screenshot={`${BACKEND_URL}${article.attributes.seo.metaImage.data.attributes.formats.medium.url}`} tech={[]}/>
-        ))}
-      </ul>
+      {articles.data.length === 0 && <EmptySection />}
+      {articles.data.length > 0 && (
+        <>
+          <ul className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {articles.data.map((article) => (
+              <WorkCard
+                variant="blog"
+                key={article.id}
+                headline={article.attributes.title}
+                description={article.attributes.seo?.metaDescription || ""}
+                link={`/blog/${article.attributes.slug}`}
+                screenshot={`${BACKEND_URL}${
+                  article.attributes.seo?.metaImage.data.attributes.url ||
+                  "/upload/70674f63fc3904c20de0.svg"
+                }`}
+                tech={[]}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </section>
   );
 };
