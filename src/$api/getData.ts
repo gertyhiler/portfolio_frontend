d25f43adba35a909d8eb4bb885012da2940b2a15
gertyhiler <tryuserrus@gmail.com>
2024-01-20 23:13:24 +0300

    formatting code

--- src/$api/getData.ts
+++ src/$api/getData.ts
@@ -1,4 +1,11 @@
-import {ProfileResponse, WorkResponse, PagesResponse, ArticlesResponse, Article, ArticleResponse} from '@/types/api';
+import {
+  ProfileResponse,
+  WorkResponse,
+  PagesResponse,
+  ArticlesResponse,
+  Article,
+  ArticleResponse,
+} from "@/types/api";
 
 export const API_URL = process.env.API_URL || "http://localhost:1337";
 const REVALIDATE_TIME = 600;
@@ -16,27 +23,25 @@ export async function getData(): Promise<WorkResponse[]> {
 }
 
 export async function getProfile(): Promise<ProfileResponse> {
-  const res = await fetch(
-    `${API_URL}/api/profile?populate=*`,
-    { next: { revalidate: REVALIDATE_TIME } }
-  );
+  const res = await fetch(`${API_URL}/api/profile?populate=*`, {
+    next: { revalidate: REVALIDATE_TIME },
+  });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
-  return (await res.json());
+  return await res.json();
 }
 
 export async function getPages(): Promise<PagesResponse> {
-  const res = await fetch(
-    `${API_URL}/api/pages?populate[seo][populate]=*`,
-    { next: { revalidate: REVALIDATE_TIME } }
-  );
+  const res = await fetch(`${API_URL}/api/pages?populate[seo][populate]=*`, {
+    next: { revalidate: REVALIDATE_TIME },
+  });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
-  return (await res.json());
+  return await res.json();
 }
 
 export async function getArticles(): Promise<ArticlesResponse> {
@@ -48,17 +53,15 @@ export async function getArticles(): Promise<ArticlesResponse> {
     throw new Error("Failed to fetch data");
   }
 
-  return (await res.json());
+  return await res.json();
 }
 export async function getArticle(slug: string): Promise<ArticleResponse> {
-  const res = await fetch(
-    `${API_URL}/api/articles/${slug}`,
-    { next: { revalidate: REVALIDATE_TIME } }
-  );
+  const res = await fetch(`${API_URL}/api/articles/${slug}`, {
+    next: { revalidate: REVALIDATE_TIME },
+  });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
-  return (await res.json());
+  return await res.json();
 }
-
